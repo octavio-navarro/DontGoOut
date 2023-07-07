b43ce43da8ed2f7580c25f46ee37d3fbe28110e8
Gilberto Echeverria <gilbertoef@gmail.com>
2023-07-06 20:35:08 -0600

    Initial player motion with lamp

---
+++ DontGoGame/Assets/Code/Scripts/CharacterMotion.cs
@@ -0,0 +1,45 @@
+/*
+Basic movement of a character on the XY plane
+
+Gilberto Echeverria
+2023-07-06
+*/
+
+using UnityEngine;
+
+[RequireComponent (typeof(Rigidbody2D))]
+public class CharacterMotion : MonoBehaviour
+{
+    [SerializeField] float speed;
+    [SerializeField] Transform lamp;
+    [SerializeField] Vector3 lampOffset;
+
+    Rigidbody2D rb2d;
+    Animator animator;
+    Vector3 move;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();        
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move.x = Input.GetAxisRaw("Horizontal");
+        move.y = Input.GetAxisRaw("Vertical");
+
+        // Normalize the vector to avoid faster diagonal movement
+        move.Normalize();
+
+        animator.SetFloat("move_x", move.x);
+        animator.SetFloat("move_y", move.y);
+
+        // Place the lamp in front of the player
+        lamp.position = transform.position + Vector3.Scale(move, lampOffset);
+
+        rb2d.velocity = move * speed;
+    }
+}
