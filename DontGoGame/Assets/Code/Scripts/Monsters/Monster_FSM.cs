43d66a341379ee81cc5e28a773d3ef2a44d0ff16
Octavio Navarro <octavio.navarro@tec.mx>
2023-07-12 00:51:40 -0600

    First version of the FSM for the monsters. Still need to include the light effect and the damage.

--- DontGoGame/Assets/Code/Scripts/Monsters/Monster_FSM.cs
+++ DontGoGame/Assets/Code/Scripts/Monsters/Monster_FSM.cs
@@ -1,50 +1,145 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Monster_FSM : MonoBehaviour
 {
+    enum States
+    {
+        Idle, Search, Chase, Attack
+    }
+
     [SerializeField] float rayLength = 1.0f;
     [SerializeField] Transform targetPosition;
-
+    [SerializeField] Transform originPosition;
+    [SerializeField] States currentState = States.Idle;
+    [SerializeField] float waitTillNextChase = 2f, waitTillNextSearch = 3f, closeDistance = 7f;
+    [SerializeField] Vector3 currentTarget;
     Vector3 rayDirection = Vector3.right;
+
+    NavMeshAgent agent;
+
+    Animator animator;
+
+    bool playerLos = false, close = false;
+
+    IEnumerator waitSearchCoroutine = null, updateTargetCoroutine = null;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
+        animator.SetInteger("State", (int)currentState);
+
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+
+        updateTargetCoroutine = UpdateTarget();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 targetDirection = targetPosition.position - transform.position;
-        rayDirection = targetDirection.normalized;    
+        Vector3 targetDirection = targetPosition.position - originPosition.position;
+        rayDirection = targetDirection.normalized;
+
+        switch(currentState)
+        {
+            case States.Idle:
+                if(waitSearchCoroutine == null)
+                {
+                    waitSearchCoroutine = WaitTillNextChase();
+                    StartCoroutine(waitSearchCoroutine);
+                }
+                break;
+
+            case States.Search:
+                UpdateSearch();
+                break;
+
+            case States.Chase:
+                UpdateChase();
+                break;
+                
+            case States.Attack:
+                UpdateAttack();
+                break;
+        }
+
+        animator.SetInteger("State", (int)currentState);
+    }
+
+    IEnumerator WaitTillNextChase()
+    {
+        yield return new WaitForSeconds(waitTillNextChase);
+        currentState = States.Search;
+        StartCoroutine(updateTargetCoroutine);
+    }
+
+    IEnumerator UpdateTarget()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(waitTillNextSearch);
+            currentTarget = targetPosition.position;
+            agent.SetDestination(currentTarget);
+        }
+    }
+
+    void UpdateSearch()
+    {
+        float distance = Vector3.Distance(transform.position, targetPosition.position);
+        
+        if(playerLos && distance <= closeDistance)
+        {
+            currentState = States.Chase;
+            StopCoroutine(updateTargetCoroutine);
+        }
+    }
+
+    void UpdateChase()
+    {
+        agent.SetDestination(targetPosition.position);
+
+        if(!playerLos)
+        {
+            currentState = States.Search;
+            StartCoroutine(updateTargetCoroutine);
+        }
+        else if(Vector3.Distance(transform.position, targetPosition.position) < 1.0f)
+            currentState = States.Attack;
+    }
+
+    void UpdateAttack()
+    {
+        Debug.Log("Attack");
+        waitSearchCoroutine = null;
+        currentState = States.Idle;
     }
 
     private void FixedUpdate() 
     {
         Physics2D.queriesHitTriggers = true;
-        RaycastHit2D[] hit = Physics2D.RaycastAll(transform.position, rayDirection, rayLength, LayerMask.GetMask("Player", "Obstacles"));
-        bool playerHit = false;
+        RaycastHit2D[] hit = Physics2D.RaycastAll(originPosition.position, rayDirection, rayLength, LayerMask.GetMask("Player", "Obstacles"));
 
         if (hit.Length > 0)
         {
-            foreach(RaycastHit2D h in hit)
-            {
-                Debug.Log(h.collider.name);
-            }
-
             if(hit[0].collider.CompareTag("Player"))
             {
-                playerHit = true;
+                playerLos = true;
             }
             else
             {
-                playerHit = false;
+                playerLos = false;
             }
         }
+        else
+        {
+            playerLos = false;
+        }
 
-        Debug.DrawRay(transform.position, rayDirection * rayLength, playerHit ? Color.green : Color.red);
+        Debug.DrawRay(originPosition.position, rayDirection * rayLength, playerLos ? Color.green : Color.red);
     }
 }
