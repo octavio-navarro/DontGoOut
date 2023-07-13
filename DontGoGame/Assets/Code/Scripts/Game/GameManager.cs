1e4989bec08c55be9b73d890ed5445d838b8bef2
Gilberto Echeverria <gilbertoef@gmail.com>
2023-07-12 18:55:04 -0600

    Return to the right position on the MainMap

--- DontGoGame/Assets/Code/Scripts/Game/GameManager.cs
+++ DontGoGame/Assets/Code/Scripts/Game/GameManager.cs
@@ -9,11 +9,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering.Universal;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] Light2D globalLight;
     [SerializeField] float globalLightIntensity = 0.1f;
+
+    [SerializeField] Transform[] houseRestartPositions;
+
+    void Awake()
+    {
+        // Check if we are getting back from a house
+        if (SceneManager.GetActiveScene().name == "MainMap") {
+            // Get the index of the house to enter, or the default position
+            int houseIndex = PlayerPrefs.GetInt("HouseIndex", 0);
+            // Set the position of the player
+            GameObject player = GameObject.FindWithTag("Player");
+            player.transform.position = houseRestartPositions[houseIndex].position;
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
--- DontGoGame/Assets/Code/Scripts/Game/SceneChanges.cs
+++ DontGoGame/Assets/Code/Scripts/Game/SceneChanges.cs
@@ -11,10 +11,17 @@ using UnityEngine.SceneManagement;
 public class SceneChanges : MonoBehaviour
 {
     [SerializeField] string sceneName = "MainMap";
+    [SerializeField] int houseIndex = 0;
+    [SerializeField] bool fromMainMap = true;
 
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player")) {
+            if (fromMainMap) {
+                // Store the reference to the house entered
+                PlayerPrefs.SetInt("HouseIndex", houseIndex);
+            }
+            // Change to the new scene
             SceneManager.LoadScene(sceneName);
         }
     }
